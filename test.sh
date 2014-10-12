fb13e3be3b9deec60efefab15536b517e4ac9d80
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-12 16:58:37 +0200

    Test script: show diff

--- test.sh
+++ test.sh
@@ -3,10 +3,13 @@
 if( [[ $# -ge 2 ]] ) then
 	./$1 < sample$2.in > output$2;
 	cat output$2;
+  echo "----- Diff -----";
+  diff output$2 sample$2.out;
 else
   # Default test
-	./$1 < sample.in > output;
-	cat output;
+  ./$1 < sample.in > output;
+  cat output;
+  echo "----- Diff -----";
+  diff output sample.out;
 fi;
 
-#diff output < output_ref
