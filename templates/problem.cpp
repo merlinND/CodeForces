5c6bb59350b5d895e76d1cb472e98ee79ecf828e
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-12 15:02:39 +0200

    Add basic C++ solution template

---
+++ templates/problem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int ll;
+
+int main() {
+	ll n;
+	cin >> n;
+
+	for(int i = 0; i < n; ++i){
+		ll x;
+		cin >> x;
+		cout << x << endl;
+	}
+
+	return 0;
+}
