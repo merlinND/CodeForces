5d526cc980cf083a755388acd248a6649cd71399
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-19 12:20:19 +0200

    towers: another wrong approach

--- 274/towers/towers.cpp
+++ 274/towers/towers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
+#include <map>
 // #include <bits/stdc++.h>
 using namespace std;
 #define REP(i,a,b) for (int i = (a); i <= (b); ++i)
@@ -19,34 +19,53 @@ using namespace std;
 #define fi first
 #define se second
 
+typedef multimap<int, int> mii;
+
 int main () {
   int n, k, a;
   cin >> n; cin >> k;
-  vi towers;
-  // Note that `lowest` contains negative heights
-  priority_queue<int> highest, lowest;
+  // Multimap height -> index of the tower
+  // Maps are sorted on the key
+  mii heights;
   FOR(i, n) {
     cin >> a;
-    towers.pb(a);
-    highest.push(a);
-    lowest.push(-a);
+    heights.insert(mp(a, i + 1));
   }
 
-  int diff, movesUsed = 0;
-  vector<pii> moves;
+  int diff = 1, diff2, nMoves = 0;
+  // List of moves (from -> to) -> number of repetitions
+  vector<pair<pii, int> > moves;
 
-  // @warning Edge case where the minimum difference is 1
-  while(moves.size() < k && diff > 0) {
-    int top = highest.top();
-    int bottom = - lowest.top();
-    diff = top - bottom;
-    moves.pb(mp(top, bottom)); // Wrong, we should indicate the indices
+  // TODO: edge case where the minimum difference is 1
+  // TODO: edge case where we have only 1 tower
+  while(nMoves < k && diff > 0) {
+    mii::iterator top = heights.end(),
+                  top2 = heights.end()--,
+                  bottom = heights.begin(),
+                  bottom2 = heights.begin()++;
+
+    diff = abs(top->fi - bottom->fi);
+    diff2 = 0;
+    if(top2 != bottom && bottom2 != top) {
+      diff2 = abs(top2->fi - bottom2->fi);
+    }
+
+    int repetitions = min(diff - diff2 + 1, k - nMoves);
+    nMoves += repetitions;
+    moves.pb(mp(mp(top->se, bottom->se), repetitions));
+
+    // Update the heights
+    heights.insert(mp(top->fi - repetitions, top->se));
+    heights.insert(mp(bottom->fi + repetitions, bottom->se));
+    heights.erase(top);
+    heights.erase(bottom);
+    diff -= repetitions;
   }
 
   cout << diff << " " << moves.size() << endl;
   FOREACH(move, moves) {
     FOR(i, move->se) {
-      cout << move->fi << " " << move->fi << endl;
+      cout << move->fi.fi << " " << move->fi.se << endl;
     }
   }
   return 0;
