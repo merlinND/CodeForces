0c88ae3549543dbd50c619281ba54bb5d50649d4
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-19 11:10:54 +0200

    expression: solution & test case

--- 274/expression/expression.cpp
+++ 274/expression/expression.cpp
@@ -18,5 +18,24 @@ using namespace std;
 #define se second
 
 int main () {
+  int maxResult;
+  int a, b, c;
+  cin >> a; cin >> b; cin >> c;
+
+  if(1 / (float)a + 1 / (float)b > 1) {
+    maxResult = a + b;
+  }
+  else {
+    maxResult = a * b;
+  }
+
+  if(1 / (float)maxResult + 1 / (float)c > 1) {
+    maxResult = maxResult + c;
+  }
+  else {
+    maxResult = maxResult * c;
+  }
+
+  cout << maxResult << endl;
   return 0;
 }
