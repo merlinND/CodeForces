a702152de1e075cbf1c30f6365b7e1b61571fc9c
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-28 21:02:02 +0100

    Simplify test script

--- test.sh
+++ test.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 if( [[ $# -ge 2 ]] ) then
-	./bin/$1 < $1$2.in > $1$2.run;
-	cat $1$2.run;
-  echo "----- Diff -----";
-  diff $1$2.run $1$2.out;
-  rm $1$2.run;
+	./bin/$1 < $1$2.in;
 else
   # Default test
-  ./bin/$1 < $1.in > $1.run;
-  cat $1.run;
-  echo "----- Diff -----";
-  diff $1.run $1.out;
-  rm $1.run;
+  ./bin/$1 < $1.in;
 fi;
 
