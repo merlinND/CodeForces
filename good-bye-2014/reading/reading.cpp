c7ff4fff6a19657babb3ec5923ba0e48a9f788ff
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-30 18:26:23 +0100

    C: input handling & cost computation

--- good-bye-2014/reading/reading.cpp
+++ good-bye-2014/reading/reading.cpp
@@ -1,14 +1,71 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <stack>
 using namespace std;
 
+#define pii pair<int, int>
+#define mp make_pair
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<bool> vb;
+
+ll computeCost(vi const & booksOrder, vi const & weights, vi const & readingOrder) {
+  ll totalEffort = 0;
+
+  stack<pii> s;
+  vector<pii> tmp;
+  // Initial stack
+  for(int i = 0; i < (int)booksOrder.size(); ++i) {
+    s.push(mp(booksOrder[i], weights[booksOrder[i]]));
+  }
+
+  for(int i = 0; i < (int)readingOrder.size(); ++i) {
+    // Find desired book in stack
+    pii p = s.top();
+    while(p.first != readingOrder[i]) {
+      tmp.push_back(p);
+      totalEffort += p.second;
+      s.pop();
+      if(!s.empty()) {
+        p = s.top();
+      }
+    }
+
+    // Found the book, we'll place it back on top
+    // Restore the stack (keeping the order)
+    for(int j = 0; j < (int)tmp.size(); ++j) {
+      s.push(tmp[j]);
+    }
+
+    s.push(p);
+    tmp.clear();
+  }
+
+  return totalEffort;
+}
 
 int main() {
+  int nBooks, nDays;
+  cin >> nBooks >> nDays;
 
+  vi weights, readingOrder;
+  /**
+   * i => index of the book in position i in the initial stack
+   * (numbered from bottom to top)
+   */
+  vi booksOrder;
+  int c;
+  for(int i = 0; i < nBooks; ++i) {
+    cin >> c;
+    weights.push_back(c);
+    booksOrder.push_back(i);
+  }
+  for(int i = 0; i < nDays; ++i) {
+    cin >> c;
+    readingOrder.push_back(c - 1);
+  }
 
+  // TODO: optimize book order
+
+  cout << computeCost(booksOrder, weights, readingOrder) << endl;
   return 0;
 }
