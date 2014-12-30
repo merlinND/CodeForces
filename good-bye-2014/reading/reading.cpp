3269ba323f618e283f462995757abd8c89c9a956
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-30 17:40:54 +0100

    C: stubs

---
+++ good-bye-2014/reading/reading.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+
+int main() {
+
+
+  return 0;
+}
