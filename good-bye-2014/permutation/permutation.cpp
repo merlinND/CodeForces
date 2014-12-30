32932be614a50386b28e3d0ae87496ccf19d45cc
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-12-30 17:28:53 +0100

    B: draft solution (overcomplicated & inefficient)

--- good-bye-2014/permutation/permutation.cpp
+++ good-bye-2014/permutation/permutation.cpp
@@ -1,11 +1,104 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+
+bool canSwap(vector<vb> const & allowed, int source, int destination) {
+  if(source == destination) {
+    return false;
+  }
+  else if(allowed[source][destination]) {
+    return true;
+  }
+
+  // Still, we might find a path (using transitivity of swaps)
+  // Explore using breadth-first search
+  int n = allowed.size();
+  vb visited;
+  for(int i = 0; i < n; ++i) {
+    visited.push_back(false);
+  }
+
+  queue<int> candidates;
+  candidates.push(source);
+  int current = source;
+  while(current != destination && !candidates.empty()) {
+    int current = candidates.front();
+    candidates.pop();
+    visited[current] = true;
+
+    // Add neighbors
+    for(int j = 0; j < n; ++j) {
+      if(allowed[current][j] && !visited[j]) {
+        candidates.push(j);
+      }
+    }
+  }
+
+  return visited[destination];
+}
 
 int main() {
+  int n;
+  cin >> n;
+
+  vi permutation;
+  int p;
+  for(int i = 0; i < n; ++i) {
+    cin >> p;
+    permutation.push_back(p);
+  }
+
+  vector<vb> allowed;
+  char a;
+  for(int i = 0; i < n; ++i) {
+    allowed.push_back(vb());
+    for(int j = 0; j < n; ++j) {
+      cin >> a;
+      allowed[i].push_back(a == '1');
+    }
+  }
+
+  // Extend paths using transitivity
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < n; ++j) {
+      if(i != j && !allowed[i][j]) {
+        allowed[i][j] = canSwap(allowed, i, j);
+      }
+    }
+  }
+
+  // We're trying to minimize the lexiographical order.
+  // Since n <= 300, we can run exhaustive search
+  bool changed;
+  do {
+    changed = false;
+
+    // For each element
+    for(int i = 0; i < n; ++i) {
+      // Find the best available "prettier" position
+      int j = 0;
+      while(j < n && (permutation[j] < permutation[i] || !allowed[i][j])) {
+        j++;
+      }
+      // Found a suitable position
+      if(j < i) {
+        changed = true;
+        swap(permutation[i], permutation[j]);
+      }
+    }
+  } while(changed);
+
+
+  cout << permutation[0];
+  for(int i = 1; i < n; ++i) {
+    cout << " " << permutation[i];
+  }
+  cout << endl;
 
   return 0;
 }
