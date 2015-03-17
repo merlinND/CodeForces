16622ea1ef7ddcd5ad17e0a48938349b8f1f648d
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-03-17 18:11:56 +0100

    B: hamming distance

--- 296/correct/correct.cpp
+++ 296/correct/correct.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+typedef long long ll;
+
+ll hammingDistance(string a, string b) {
+  ll distance = 0;
+  for(size_t i = 0; i < a.length(); ++i) {
+    if(a[i] != b[i]) {
+      distance++;
+    }
+  }
+  return distance;
+}
+
 
 int main() {
+  ll length;
+  string first, second;
+  cin >> length;
+  cin >> first >> second;
+
+  // TODO:
+  ll minDistance = hammingDistance(first, second);
+  ll pos1 = -1, pos2 = -1;
+
+  cout << minDistance << endl << pos1 << " " << pos2 << endl;
   return 0;
 }
