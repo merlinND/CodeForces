940fc0382811961516348911897925281468187d
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-03-17 18:30:46 +0100

    B: solution draft

--- 296/correct/correct.cpp
+++ 296/correct/correct.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 typedef long long ll;
+
+vector<ll> getDifferences(string a, string b) {
+  vector<ll> differences;
+  for(size_t i = 0; i < a.length(); ++i) {
+    if(a[i] != b[i]) {
+      differences.push_back(i);
+    }
+  }
+  return differences;
+}
 
 ll hammingDistance(string a, string b) {
   ll distance = 0;
@@ -13,6 +24,10 @@ ll hammingDistance(string a, string b) {
   return distance;
 }
 
+ll trySwap(string a, string b, ll i, ll j) {
+  swap(a[i], a[j]);
+  return hammingDistance(a, b);
+}
 
 int main() {
   ll length;
@@ -20,10 +35,31 @@ int main() {
   cin >> length;
   cin >> first >> second;
 
-  // TODO:
-  ll minDistance = hammingDistance(first, second);
-  ll pos1 = -1, pos2 = -1;
+  // Find the single swap in `first`
+  // which minimizes its hamming distance to `second`
+  vector<ll> differences = getDifferences(first, second);
+  ll minDistance = differences.size();
+  ll pos1 = -2, pos2 = -2;
 
-  cout << minDistance << endl << pos1 << " " << pos2 << endl;
+  if(minDistance > 1) {
+    // Try each swap among the differences
+    // (there's no sense swapping letters which are in place)
+    vector<ll>::iterator diff1, diff2;
+    for(diff1 = differences.begin(); diff1 != differences.end(); ++diff1) {
+      for(diff2 = diff1; diff2 != differences.end(); ++diff2) {
+        ll i = (*diff1);
+        ll j = (*diff2);
+        ll d = trySwap(first, second, i, j);
+        // cout << d << ", " << i << " " << j << endl;
+        if(a[i] != b[j] && d < minDistance) {
+          minDistance = d;
+          pos1 = i;
+          pos2 = j;
+        }
+      }
+    }
+  }
+
+  cout << minDistance << endl << (pos1+1) << " " << (pos2+1) << endl;
   return 0;
 }
