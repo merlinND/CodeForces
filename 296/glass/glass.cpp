1663f56f52638303e11b60f769336f23e959824d
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-03-17 19:45:57 +0100

    C: disable output to gain speed (solution still incorrect)

--- 296/glass/glass.cpp
+++ 296/glass/glass.cpp
@@ -7,12 +7,6 @@ typedef long long ll;
 typedef pair<long, long> pll;
 
 void printArea(pll const & vertical, pll const & horizontal) {
-  cerr << "Best rectangle: "
-       << vertical.first << " ... " << vertical.second
-       << " x "
-       << horizontal.first << " ... " << horizontal.second
-       << " --> ";
-
   cout << (vertical.second - vertical.first)
         * (horizontal.second - horizontal.first) << endl;
 }
@@ -22,8 +16,7 @@ void updateLargestInterval(vector<ll> & cuts, pll & bestInterval) {
   sort(cuts.begin(), cuts.end());
 
   for(size_t i = 1; i < cuts.size(); ++i) {
-    cerr << "Proposed interval: " << cuts[i-1] << " ... " << cuts[i] << endl;
-    if(cuts[i] - cuts[i-1] > best) {
+    if(cuts[i] - cuts[i-1] >= best) {
       bestInterval.first = cuts[i-1];
       bestInterval.second = cuts[i];
       best = (bestInterval.second - bestInterval.first);
@@ -50,21 +43,17 @@ int main() {
     cin >> type >> point;
     if(type == 'V') {
       vCuts.push_back(point);
-      cerr << "New V cut at " << point << endl;
       // Affected our best area
       if(point > vBest.first && point < vBest.second) {
         // Need to find the new best vertical interval
-        cerr << "Spoiled " << vBest.first << " ... " << vBest.second << endl;
         updateLargestInterval(vCuts, vBest);
       }
     }
     else {
       hCuts.push_back(point);
-      cerr << "New H cut at " << point << endl;
       // Affected our best area
       if(point > hBest.first && point < hBest.second) {
         // Need to find the new best vertical interval
-        cerr << "Spoiled " << hBest.first << " ... " << hBest.second << endl;
         updateLargestInterval(hCuts, hBest);
       }
     }
