7d27417bcfdd6532c311e1c80644b2a29dc49429
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-03-20 18:16:58 +0100

    B: change data representation for more efficient solution (draft)

--- 296/correct/correct.cpp
+++ 296/correct/correct.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <vector>
+#include <map>
 using namespace std;
 
 typedef long long ll;
+/** Multimap (a wrong letter -> its position) */
+typedef multimap<char, ll> differences_t;
 
-vector<ll> getDifferences(string a, string b) {
-  vector<ll> differences;
+
+differences_t getDifferences(string a, string b) {
+  differences_t differences;
   for(size_t i = 0; i < a.length(); ++i) {
     if(a[i] != b[i]) {
-      differences.push_back(i);
+      differences.insert(make_pair<>(a[i], i));
     }
   }
   return differences;
@@ -24,9 +27,12 @@ ll hammingDistance(string a, string b) {
   return distance;
 }
 
-ll trySwap(string a, string b, ll i, ll j) {
+ll trySwap(string a, string b, ll i, ll j, ll distance) {
   swap(a[i], a[j]);
-  return hammingDistance(a, b);
+  // Distance can only improve by 0, 1 or 2
+  if(a[i] == b[i]) distance--;
+  if(a[j] == b[j]) distance--;
+  return distance;
 }
 
 void printResults(ll finalDistance, ll pos1 = -2, ll pos2 = -2) {
@@ -39,43 +45,62 @@ int main() {
   cin >> length;
   cin >> first >> second;
 
-  // Find the single swap in `first`
+  // Objective: find the single swap in `first`
   // which minimizes its hamming distance to `second`
-  vector<ll> differences = getDifferences(first, second);
   ll initialDistance = hammingDistance(first, second);
-  ll minDistance = initialDistance;
-  ll pos1 = -2, pos2 = -2;
 
-  if(minDistance <= 1) {
-    printResults(minDistance);
+  // We can't improve using a swap if there are less that 2 wrong letters
+  if(initialDistance <= 1) {
+    printResults(initialDistance);
     return 0;
   }
 
+  // Try collecting one or two 'wrong' letters which are desired elsewere
+  ll pos1 = -1, pos2 = -1;
+  differences_t differences = getDifferences(first, second);
 
-  // Try each swap among the differences
-  // (there's no sense swapping letters which are in place)
-  vector<ll>::iterator diff1, diff2;
-  for(diff1 = differences.begin(); diff1 != differences.end(); ++diff1) {
-    for(diff2 = diff1; diff2 != differences.end(); ++diff2) {
-      ll i = (*diff1);
-      ll j = (*diff2);
-      ll d = trySwap(first, second, i, j);
-      // cout << d << ", " << i << " " << j << endl;
-      if(first[i] != first[j] && d < minDistance) {
-        minDistance = d;
-        pos1 = i;
-        pos2 = j;
-      }
+  differences_t::iterator it, range, rangeEnd;
+  pair<differences_t::iterator, differences_t::iterator> pIt;
+  for(it = differences.begin(); it != differences.end(); ++it) {
+    // At this position, what we have VS what we would like
+    char available = it->first;
+    char desired = second[it->second];
 
-      // We know there's no way to get better than a -2 improvement
-      // with a single swap
-      if(minDistance <= initialDistance - 2) {
-        printResults(minDistance, pos1, pos2);
-        return 0;
-      }
+    // See if this desired letter is available among the other candidates
+    pIt = differences.equal_range(desired);
+    range = pIt.first;
+    rangeEnd = pIt.second;
+
+    if(range != rangeEnd) {
+      // Cool, now check if any of these candidates
+      // actually are waiting for the letter we have available
+      do {
+        pos1 = range->second;
+        if(second[range->second] == available) {
+          // We know there's no way to get better than a -2 improvement
+          // with a single swap
+          pos2 = it->second;
+          printResults(initialDistance - 2, pos1, pos2);
+          return 0;
+        }
+
+        range++;
+      } while(range != rangeEnd);
     }
   }
 
-  printResults(minDistance, pos1, pos2);
+  // We still found at least one useful letter, swap it with any other
+  if(pos1 >= 0) {
+    differences_t::iterator it = differences.begin();
+    do {
+      pos2 = it->second;
+      it++;
+    } while(pos2 == pos1);
+
+    printResults(initialDistance - 1, pos1, pos2);
+    return 0;
+  }
+
+  printResults(initialDistance);
   return 0;
 }
