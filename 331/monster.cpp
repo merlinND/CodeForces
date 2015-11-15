26c7008f0fa27da7282bdf83ef45c5ddc2712176
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-11-15 17:25:43 +0100

    Add starter files

---
+++ 331/monster.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+using ll = long long int;
+
+int main() {
+  ll n, r;
+  cin >> n;
+  r = (n - 2) * (n - 2);
+  cout << r << endl;
+  return 0;
+}
