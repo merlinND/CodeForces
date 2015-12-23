d4b003dd64ef176159db140559b1b532f53367ec
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-12-23 19:04:34 +0100

    Problem B (queue based solution with memoization)

--- 336/hamming.cpp
+++ 336/hamming.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <deque>
+
 using namespace std;
 
 using ll = long long int;
 
-inline int cmp(char aa, char bb) {
-  return (aa == bb ? 0 : 1);
+namespace {
+  map<deque<bool>, ll> sums;
+}
+
+inline ll cmp(const vector<bool> & aa, const deque<bool> & window) {
+  if (sums.count(window) <= 0) {
+    ll sum = 0;
+    size_t i = 0;
+    for (const bool & w : window) {
+      sum += (aa[i] == w ? 0 : 1);
+      ++i;
+    }
+    sums[window] = sum;
+    return sum;
+  }
+  return sums[window];
 }
 
 int main() {
-  string a, b;
-  cin >> a; cin >> b;
+  string aa, bb;
+  cin >> aa; cin >> bb;
+  size_t n = aa.length(), nb = bb.length();
 
-  if (b.length() < a.length()) {
-    cout << 0 << endl;
-    return 0;
+  vector<bool> a(n);
+  deque<bool> w;
+  // Prepare initial window
+  for (size_t i = 0; i < n; ++i) {
+    a[i] = (aa[i] == '1');
+    w.push_back(bb[i] == '1');
   }
 
-  ll s = 0;
-
+  ll s = cmp(a, w);
   // Add up the partial sums incrementally
-  for (ll offset = 0; offset + a.length() <= b.length(); ++offset) {
-    for (ll i = 0; i < a.length(); ++i) {
-      s += cmp(a[i], b[offset + i]);
-    }
+  for (ll offset = 1; offset + n <= nb; ++offset) {
+    w.pop_front();
+    w.push_back(bb[offset + n - 1] == '1');
+
+    s += cmp(a, w);
   }
 
   cout << s << endl;
